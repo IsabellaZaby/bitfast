f5e88849eab1dd07214f85f140334e4e39ef16b0
Isabella Zaby <isabella.zaby@gmail.com>
2023-05-22 22:00:19 +0200

    changes after fourth measurement

--- src/client/components/common/navigation/Navigation.tsx
+++ src/client/components/common/navigation/Navigation.tsx
@@ -6,54 +6,11 @@ interface INavigation {
     children: ReactNode;
 }
 
-function generateRandomStringExpensive(length: number): string {
-    let result = '';
-    const characters =
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(
-            Math.floor(Math.random() * charactersLength),
-        );
-    }
-    return result.replace(/[aeiou]/g, '');
-}
-
-// meant to do nothing but be expensive for the CPU
-const expensiveOperation = (obj: Window | NodeJS.Process) => {
-    const stringArray: string[] = [];
-    Object.keys(obj).forEach((index: string) => {
-        index.split('').forEach(() => {
-            stringArray.push(generateRandomStringExpensive(index.length));
-            stringArray.push(generateRandomStringExpensive(index.length));
-            stringArray.push(generateRandomStringExpensive(index.length));
-            stringArray.push(generateRandomStringExpensive(index.length));
-            stringArray.push(generateRandomStringExpensive(index.length));
-            stringArray.push(generateRandomStringExpensive(index.length));
-            stringArray.push(generateRandomStringExpensive(index.length));
-            stringArray.push(generateRandomStringExpensive(index.length));
-            stringArray.push(generateRandomStringExpensive(index.length));
-            stringArray.push(generateRandomStringExpensive(index.length));
-        });
-    });
-    stringArray.sort((a: string, b: string) => a.length - b.length);
-    return (
-        stringArray[stringArray.length - 1].length < Object.keys(obj).length * 2
-    );
-};
-
 const Navigation: FunctionComponent<INavigation> = ({ children }) => {
     const device = useContext(DeviceContext);
 
-    const expensive =
-        typeof window !== 'undefined'
-            ? expensiveOperation(window)
-            : expensiveOperation(process);
-
-    const dangerous = expensive && typeof window !== 'undefined';
     const classNames = [
-        dangerous ? 'client' : 'server',
-        device.isMobile ? 'mobile' : 'desktop',
+        device.isMobile ? 'mobile' : 'desktop'
     ]
         .toString()
         .replace(',', ' ');
